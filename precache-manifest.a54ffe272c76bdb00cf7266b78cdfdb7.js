e95eee7c81e4d4f3b5b060133b5342f83c3aaf8a
MiltonFLJr <miltonferlimajr@gmail.com>
2020-09-17 17:07:44 -0300

    Updates

---
+++ precache-manifest.a54ffe272c76bdb00cf7266b78cdfdb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0a53009041e81462d0a300473a4c4a",
+    "url": "/WeatherInfo/index.html"
+  },
+  {
+    "revision": "f44d269d7c968353674a",
+    "url": "/WeatherInfo/static/css/main.52fd9242.chunk.css"
+  },
+  {
+    "revision": "6548b8b6f730ace06811",
+    "url": "/WeatherInfo/static/js/2.f127b6b5.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/WeatherInfo/static/js/2.f127b6b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f44d269d7c968353674a",
+    "url": "/WeatherInfo/static/js/main.fe84aefd.chunk.js"
+  },
+  {
+    "revision": "2f2e3ea716dd26c360ad",
+    "url": "/WeatherInfo/static/js/runtime-main.a1405c6c.js"
+  },
+  {
+    "revision": "729dd90166b772c46750cf61e8fab020",
+    "url": "/WeatherInfo/static/media/weather.729dd901.png"
+  }
+]);
